2f1366e80bd8d2c4aa753b4b06f4b2026019fc61
mellgrenclaesson-code <mellgren.claesson@gmail.com>
2026-02-12 15:27:20 +0100

    Tog bort class grejer
    Removed class definition and methods related to BasicClass. Updated comments and retained the loop structure.

--- Temperaturfordelning_linalg.m
+++ Temperaturfordelning_linalg.m
@@ -4,24 +4,11 @@
 
 n = 4 
 
-classdef BasicClass
-   properties
-      Value {mustBeNumeric}
-   end
-   methods
-      function r = roundOff(obj)
-         r = round([obj.Value],2);
-      end
-      function r = multiplyBy(obj,n)
-         r = [obj.Value]*n;
-      end
-   end
-end
-
 for n=4  %Antal noder
 
 
 
 end
+
 
 M=1/n.*[t1]
